45e8b787fe24d09886a3107701bc182684a33577
Gopikrishna-G <gopikrishnag.9774@gmail.com>
2024-04-05 20:47:47 +0530

    home --> login --> registration

--- src/main/java/com/Sports/demo/Controller/UserController.java
+++ src/main/java/com/Sports/demo/Controller/UserController.java
@@ -9,23 +9,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
-
 @Controller
 public class UserController {
     @Autowired
     UserRepo userrepo;
     @GetMapping("/")
+    public String getHomePage() {
+//        List<User> users= userrepo.findAll();
+//        model.addAttribute("user", users);
+        return "Home";
+    }
+    @PostMapping("/login")
+    public String handleSelection(@ModelAttribute("user") User user, Model model) {
+        return "login";
+    }
+
+    @GetMapping("/login")
     public String getRegPage(@ModelAttribute User user) {
 //        List<User> users= userrepo.findAll();
 //        model.addAttribute("user", users);
-        return "index";
+        return "login";
     }
     @PostMapping("/registration")
     public String saveUser(@ModelAttribute("user") User user, Model model)
     {
         userrepo.save(user);
-        model.addAttribute("message","Registration successful");
+        model.addAttribute("message","Registration successful \n You will be redirected to login page");
         return "regis";
     }
 
